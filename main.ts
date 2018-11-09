d3f8dd52c14b3b17624fd6579e6fe0354a1c3ccc
Mikko Jokinen <miguit@gmail.com>
2018-11-09 14:05:53 +0200

    degree handling

--- main.ts
+++ main.ts
@@ -1,11 +1,34 @@
+enum degreeName {
+    //% block=Ylos
+    Ylos = 0,
+   //% block=Alas
+    Alas = 180,
+   //% block=Eteen
+    Eteen = 90
+}
+
 //% //% weight=5 color=#2699BF icon="\uf1b0" block="Opinsys:Nalle"
 namespace opinsys_nalle {
+
+    export class Nalle {
 
 	/************************************************************************************************************************************************
 	* micro:bit Opinsys:Nalle
 	************************************************************************************************************************************************/
+           /**
+         * Shows all LEDs to a given color (range 0-255 for r, g, b). 
+         * @param degree RGB color of the LED
+         */
+    //% blockId="opinsys_nalle_asteet" block="%nalle|asteet %degree=degreeName" 
+    //% weight=85 blockGap=8
+     setDegree(degree: number) {
+        degree = degree >> 0;
+     }
+    }
 
-
+    export function degree(degree: number): number {
+        return degree(degree);
+    }
     /**
      * Drives nalles hands down
      */
@@ -14,6 +37,12 @@ namespace opinsys_nalle {
     export function tassutAlas(): void {
         pins.servoWritePin(AnalogPin.P1, 0);
         pins.servoWritePin(AnalogPin.P2, 180);
+    }
+    //% blockId=opinsys_nalle_tassu_asteet
+    //% block="tassu asteet"
+    export function tassutAsteet(number: degree) {
+        pins.servoWritePin(AnalogPin.P1, degree);
+        pins.servoWritePin(AnalogPin.P2, degree);
     }
 
     /**
